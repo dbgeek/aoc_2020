371d9ab44fe9b7858a6bfb1f18b6966c0a0c13dc
Björn Ahl <bnal@ba78.me>
2020-12-06 14:28:27 +0100

    adding tool getInputNestedArrayString

--- ts/src-ts/tools.test.ts
+++ ts/src-ts/tools.test.ts
@@ -1,6 +1,12 @@
-import { getInputArrayString, getInputArrayNumber } from './tools';
+import { getInputArrayString, getInputNestedArrayString, getInputArrayNumber } from './tools';
 
 test('testing getInputArrayNumber', () => {
   expect(getInputArrayNumber(0, 0)).toStrictEqual([1721, 979, 366, 299, 675, 1456]);
   expect(getInputArrayString(0, 0)).toStrictEqual(['1721', '979', '366', '299', '675', '1456']);
+  expect(getInputNestedArrayString(0, 2)).toStrictEqual([
+    ['1721'],
+    ['979', '366', '299'],
+    ['675', '1456'],
+    ['xc vvb hh'],
+  ]);
 });
--- ts/src-ts/tools.ts
+++ ts/src-ts/tools.ts
@@ -18,4 +18,19 @@ function getInputArrayString(day: number, part: number): Array<string> {
     .map((v) => v);
 }
 
-export { getInput, getInputArrayString, getInputArrayNumber };
+function getInputNestedArrayString(day: number, part: number): string[][] {
+  const groups: string[][] = [];
+  const group: string[] = [];
+  getInputArrayString(day, part).forEach((v, idx, arr) => {
+    if (v === '') {
+      groups.push([...group]);
+      group.length = 0;
+    } else {
+      group.push(v);
+    }
+    if (idx === arr.length - 1 && group.length !== 0) groups.push([...group]);
+  });
+  return groups;
+}
+
+export { getInput, getInputArrayString, getInputArrayNumber, getInputNestedArrayString };
